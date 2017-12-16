a041a6bf2e69774fcf41e98aec89ce176e375bfe
ebiryu <ebiryu.zatta@gmail.com>
2017-12-16 23:57:18 +0900

    fix a bug

--- src/Main.elm
+++ src/Main.elm
@@ -38,8 +38,7 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+        [ h1 [] [ text "Your Elm App is working!" ]
         , objects model
         ]
 
